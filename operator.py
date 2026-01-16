52521de299ef57ac60230993dca58e5a313bbda4
enter your username <enter your email>
2026-01-16 12:38:23 +0000

    using operator

---
+++ operator.py
@@ -0,0 +1,17 @@
+#x = 20
+#y = 5
+# x += y  
+#x = x + y 
+#print(x)
+
+#x = 54 + 6 
+#y = 59
+#print(x+y) 
+
+a = 7
+b = 500
+if b >= 335: 
+    print(a) 
+
+
+
--- variable.py
+++ variable.py
@@ -4,8 +4,17 @@
 #lenght = len("Wilmot")
 #print(lenght)
 
-a = 5
-b = "Saybioh"
-print(a)
-print(b)
+#a = 5
+#b = "Saybioh"
+#print(a)
+#print(b)
+
+#class_name = "lux in light"
+#class_member = 14
+#class_regular_course = 6
+#class_lab = "Six hours a weeek"
+#print(class_name)
+#print(class_member)  
+#print(class_regular_course)
+#print(class_lab) 
 
