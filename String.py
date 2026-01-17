e35ad07b8862d7507efa5bbff59a1085cd567013
enter your username <enter your email>
2026-01-17 13:49:51 +0000

    using loop and strings

--- String.py
+++ String.py
@@ -8,7 +8,24 @@
 #full_name = son_name + " " + last_name
 #print(full_name)
 
-mother_name = "Marlyne Melissa"
-last_name = "Okai"
-full_name = mother_name + " " + last_name
-print(full_name)
+#mother_name = "Marlyne Melissa"
+#last_name = "Okai"
+#full_name = mother_name + " " + last_name
+#print(full_name)
+
+#Replication
+#my_birthday = "Party Celebration!"
+#print(my_birthday *10 + " ") 
+
+#my_birthday = "Party Celebration!"
+#for i in range(10):
+#print(f"{i}: {my_birthday}") 
+    
+    
+my_birthday = "Party Celebration!"
+for i in range(10):
+    print( f" {i}  {my_birthday}")
+    
+    my_birthday = "Party Celebration!"
+for i in range(10):
+    print(f"{i:>2}: {my_birthday}")
