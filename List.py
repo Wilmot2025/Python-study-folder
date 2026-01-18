29536bf584904ce4a304a7735bb7f824847fcf57
enter your username <enter your email>
2026-01-18 11:13:01 +0000

    list methods

---
+++ List.py
@@ -0,0 +1,27 @@
+#List => A list is a collection of items stored in a single variable.
+#It allows you to keep multiple values together in an ordered way.
+
+# list methods 
+
+# 1. append() => using append method , we can add any single element at the end.
+
+# Extends => using extend() method , we can add multiple element at the end of list.
+
+# 3. insert => we can add any element at specified index. 
+
+# 4. remove() => we can remove/delete any element from a list 
+
+# 5. pop() => using pop() , you can delete last index element or specified index element.
+
+## 6. index() => using this method we can find index of any element.  
+
+## 7. count() => it gives the number of occurence of specifed element in a list.
+
+## 8. sort() => by using this method, we can sort our list , in any order asending order or descending order. 
+# => by default it gives asecending order .
+
+## reverse() => by using this method , we can reverse our list. 
+
+## copy() => we can copy all elemente from one list to another list.
+
+
